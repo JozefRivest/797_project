ad618bbff1f8447da22db2b5a905e6fb93cb8954
JozefRivest <jrivest@umich.edu>
2025-09-10 20:42:30 -0400

    Changed the structure and the path of the output of the codebook

--- codebook/compiling.R
+++ codebook/compiling.R
@@ -2,5 +2,5 @@ library(knitr)
 library(xtable)
 
 # Method 1: Two step process
-knit("codebook/codebook.Rnw") # Creates example.tex
-system("pdflatex codebook.tex") # Creates example.pdf
+knit("codebook/codebook.Rnw", output = "codebook/output/codebook.tex") # Creates example.tex
+system("pdflatex -output-directory=codebook/output codebook/output/codebook.tex") # Creates example.pdf
